cbc2f1c3e9fc00a8bb559301edffba6179e8d317
ashagiro <aydanashagirova@gmail.com>
2023-02-17 18:39:44 +0600

    static added

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -13,7 +13,12 @@ func home(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	ts, err := template.ParseFiles("./ui/html/home.page.html")
+	files := []string{
+		"./ui/html/home.page.html",
+		"./ui/html/base.layout.html",
+		"./ui/html/footer.partial.html",
+	}
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
